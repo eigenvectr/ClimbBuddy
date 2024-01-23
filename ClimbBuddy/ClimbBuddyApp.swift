9ffe78cb20bc9ec54fccd75934e859b8a907adc5
Edward <edmolina423@gmail.com>
2024-01-23 16:01:59 -0500

    Initial Commit

---
+++ ClimbBuddy/ClimbBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimbBuddyApp.swift
+//  ClimbBuddy
+//
+//  Created by Edward Laurence on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ClimbBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
