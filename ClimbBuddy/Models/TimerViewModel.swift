86198e9a6d661a5bf0a6b4357ea438bdbbe15b4c
Edward <edmolina423@gmail.com>
2024-02-13 16:26:29 -0500

    Implemented add and delete timer operations from the data model - addTimer method to create and save new `Tbl_Timer` and `Tbl_Exercise` entities to Core Data. - Ensure that new exercises are linked to their respective timers and that the context is saved after all related entities are added. - deleteItem method to remove `Tbl_Timer` entities from Core Data that correspond to a given `MyTimer` object, along with updating the local array. - Error handling

--- ClimbBuddy/Models/TimerViewModel.swift
+++ ClimbBuddy/Models/TimerViewModel.swift
@@ -27,17 +27,21 @@ class TimerViewModel: ObservableObject{
         do {
             // Execute the fetch request to retrieve an array of Tbl_Timer instances
             let array = try context.fetch(fetchReq)
+            
             // Iterate over each fetched Tbl_Timer object
             for i in array {
                 var exe: [MyExercise] = [] // Initialize an empty array for MyExercise
+                
                 // Attempt to cast the 'exercises' relationship to a Set of Tbl_Exercise
                 if let exercisesSet = i.exercises as? Set<Tbl_Exercise> {
                     let exercisesArray = Array(exercisesSet) // Convert the set to an array
+                    
                     // Iterate over the exercises array to populate the MyExercise array
                     for e in exercisesArray {
                         exe.append(MyExercise(name: e.name ?? "", duration: e.duration ?? ""))
                     }
                 }
+                
                 // Append a new MyTimers object to the myTimers array with data from the current Tbl_Timer
                 self.myTimers.append(MyTimer(id: i.id ?? UUID().uuidString, name: i.name ?? "", duration: i.duration ?? "", numberOfSets: i.numberOfSets ?? "0", exercises: exe))
             }
@@ -58,7 +62,61 @@ class TimerViewModel: ObservableObject{
         }
     }
     
+    func addTimer(_myTimer: MyTimer){
+        // Creating new Tbl_Timer instance in the Core Data context
+        let timerData = Tbl_Timer(context: context)
+        
+        // Setting properties of the Tbl_Timer instance from the MyTimer parameter
+        timerData.name = _myTimer.name
+        timerData.duration = _myTimer.duration
+        timerData.numberOfSets = _myTimer.numberOfSets
+        timerData.id = _myTimer.id
+        
+        // Iterate through the exercises array of the MyTimer parameter
+        for (i,obj) in _myTimer.exercises.enumerated() {
+            // Create a new Tbl_Exercise instance in the Core Data context for each exercise
+            let tmrData = Tbl_Exercise(context:context)
+            tmrData.name = obj.name
+            tmrData.duration = obj.duration
+            timerData.addToExercises(tmrData) // Add the Tbl_Exercise instance to the exercises set of the Tbl_Timer instance
+            
+            // If we are on the last exercise, save the context
+            if i == _myTimer.exercises.count - 1 {
+                saveContext()
+            }
+        }
+        
+        // Save the context after all exercises have been added to the timer
+        saveContext()
+    }
+    
+    func deleteItem(_ myTimer: MyTimer) {
+        // Set up a fetch request to find the Tbl_Timer object in Core Data by its ID
+        let fetchRequest: NSFetchRequest<Tbl_Timer> = Tbl_Timer.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %@", myTimer.id)
+        
+        do {
+            // Execute the fetch request to get an array of Tbl_Timer objects that match the predicate
+            let results = try context.fetch(fetchRequest)
+            
+            // Check if we have a result and get the first one since IDs should be unique
+            if let timerToDelete = results.first {
+                // Delete the fetched Tbl_Timer object from the Core Data context
+                context.delete(timerToDelete)
+                
+                // Remove the corresponding MyTimer object from the local array
+                myTimers.removeAll { $0.id == myTimer.id }
+                
+                // Save the context to commit the deletion in Core Data
+                try context.save()
+            }
+        } catch {
+            // Log error if timer deletion fails
+            print("Error deleting item: \(error)")
+        }
+    }
+
+    
     //updateTimers
     
-    //deleteTimers
 }
