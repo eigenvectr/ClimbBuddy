af92db253b05e3924bdfef0a82fab66aeeddbc76
Edward <edmolina423@gmail.com>
2024-02-13 14:47:04 -0500

    Added global constants and utility functions for the app -Added Constant.swift containg UI color constants, custom font, and functions for duration calculations -Defined app wide color scheme and font -Implemented sumDurations to calculate total duration from an array of exercise, "MyExercise", objects -Added sumDuratioinsInSeconds to return total duration from a string of durations

---
+++ ClimbBuddy/Helpers/Constant.swift
@@ -0,0 +1,71 @@
+//
+//  Constant.swift
+//  ClimbBuddy
+//
+//  Created by Edward Laurence on 2/13/24.
+//
+
+import Foundation
+import SwiftUI
+
+import Foundation
+import SwiftUI
+
+let dSize = UIScreen.main.bounds.size
+
+struct color {
+    
+    static let appThemeColor: Color = Color("AppThemeColor")
+    static let appCardColor : Color = Color("CardColor")
+    static let appBGColor : Color = Color("BGColor")
+    
+}
+
+
+struct CustomFont {
+    static let bold       = "Inter-Bold"
+    static let extraBold  = "Inter-ExtraBold"
+    static let semiBold   = "Inter-SemiBold"
+    static let medium     = "Inter-Medium"
+    static let regular    = "Inter-Regular"
+    static let thin       = "Inter-Thin"
+    static let light      = "Inter-Light"
+    static let extraLight = "Inter-ExtraLight"
+}
+
+func sumDurations(_ timer: [MyExercise]) -> String {
+    var totalMinutes = 0
+
+    for i in timer {
+        let components = i.duration.components(separatedBy: ":")
+        if components.count == 2,
+           let hours = Int(components[0]),
+           let minutes = Int(components[1]) {
+            totalMinutes += hours * 60 + minutes
+        }
+    }
+
+    let hours = totalMinutes / 60
+    let minutes = totalMinutes % 60
+
+    // Format the result as "HH:mm"
+    let formattedResult = String(format: "%02d:%02d", hours, minutes)
+
+    return formattedResult
+}
+
+
+func sumDurationsInSeconds(_ durations: [String]) -> Int {
+    var totalSeconds = 0
+
+    for duration in durations {
+        let components = duration.components(separatedBy: ":")
+        if components.count == 2,
+           let minutes = Int(components[0]),
+           let seconds = Int(components[1]) {
+            totalSeconds += minutes * 60 + seconds
+        }
+    }
+
+    return totalSeconds
+}
